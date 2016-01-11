ecce14cec9ccce1c28c58cf7c465e6896c1c1dbc
kehontas <kehontas@gmail.com>
2016-01-11 15:51:19 -0800

    add new columns to Beers

--- db/migrate/20160111233257_add_description_to_beers.rb
+++ db/migrate/20160111233257_add_description_to_beers.rb
@@ -1,12 +1,9 @@
 class AddDescriptionToBeers < ActiveRecord::Migration
   def change
     add_column :beers, :brewery_name, :string
-    add_column :beers, :string,, :string
     add_column :beers, :short_description, :string
-    add_column :beers, :string,, :string
     add_column :beers, :long_description, :string
-    add_column :beers, :text,, :string
     add_column :beers, :style, :string
-    add_column :beers, :string, :string
+
   end
 end
